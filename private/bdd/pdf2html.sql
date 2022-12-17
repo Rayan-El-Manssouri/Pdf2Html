98d849b5222bd5a399ff6304d0deff119ea6362f
Rayan-El-Massouri <liviackerman69@gmail.com>
2022-12-17 23:15:59 +0100

    Mise a jour du fichier sql

--- private/bdd/pdf2html.sql
+++ private/bdd/pdf2html.sql
@@ -3,11 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : lun. 05 déc. 2022 à 11:07
+-- Généré le : sam. 17 déc. 2022 à 22:14
 -- Version du serveur : 5.7.36
 -- Version de PHP : 7.4.26
-
-
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -22,6 +20,7 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `pdf2html`
 --
+
 CREATE DATABASE pdf2html;
 
 USE pdf2html;
@@ -35,9 +34,22 @@ USE pdf2html;
 DROP TABLE IF EXISTS `image`;
 CREATE TABLE IF NOT EXISTS `image` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `src` varchar(60) NOT NULL,
+  `src` varchar(200) NOT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=MyISAM AUTO_INCREMENT=42 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=561 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `image_stockage`
+--
+
+DROP TABLE IF EXISTS `image_stockage`;
+CREATE TABLE IF NOT EXISTS `image_stockage` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `src` varchar(200) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
